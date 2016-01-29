08baec592984ba550cf8a85c9ba2b87ef19c7352
ghazal <helmzadeh.ghazal@gmail.com>
2016-01-29 17:40:39 +0330

    first version of the app

---
+++ src/com/example/dongi/dongiActivity.java
@@ -0,0 +1,15 @@
+package com.example.dongi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class dongiActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
