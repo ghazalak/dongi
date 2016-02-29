f11fdae23aa11c92a6c72d378798d4b334935dd1
ghazal <helmzadeh.ghazal@gmail.com>
2016-02-29 18:25:29 +0330

    edit rows and NumberPicker

--- src/com/example/dongi/contactsActivity.java
+++ src/com/example/dongi/contactsActivity.java
@@ -1,27 +1,39 @@
 package com.example.dongi;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 
 public class contactsActivity extends Activity{
-NumberPicker.
-//    Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-
-//    if (cursor != null) {
-//        while(cursor.moveToNext()) {
-//            String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-//            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-//        }
-//    }
     @Override
     public void onCreate(Bundle bdl){
         super.onCreate(bdl);
         setContentView(R.layout.contact);
+        ActionBar actionBar=getActionBar();
+        LayoutInflater inflater=getLayoutInflater();
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+        View actionbar_view=inflater.inflate(R.layout.actionbar_layout,null);
+        actionBar.setCustomView(actionbar_view);
+        actionBar.setDisplayShowCustomEnabled(true);
+//        View rootView = inflater.inflate(R.layout.contact, container, false);
+//        NumberPickerCustom np = (NumberPickerCustom) NumberPickerCustom.findViewById(R.id.numberPicker1);
+//        np.setOnValueChangedListener(this);
+
+    }
+//    @Override
+//    public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+//        Toast.makeText(contactsActivity.this, "number", Toast.LENGTH_SHORT).show();
+//    }
+    public void btnBackToFoodList(View view ) {
+        Intent returnIntent = new Intent();
+        setResult(Activity.RESULT_CANCELED, returnIntent);
+        finish();
+
     }
 }
