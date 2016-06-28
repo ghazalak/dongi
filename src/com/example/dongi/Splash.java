e969b37a2ba7a3f88de30b6ea22c3b0f79870bcf
ghazal <helmzadeh.ghazal@gmail.com>
2016-06-28 11:05:50 +0430

    splash form added

---
+++ src/com/example/dongi/Splash.java
@@ -0,0 +1,22 @@
+package com.example.dongi;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+public class Splash extends Activity {
+    @Override
+    public void onCreate(Bundle bdl) {
+        super.onCreate(bdl);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this,DongiActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, 5000);
+    }
+}
